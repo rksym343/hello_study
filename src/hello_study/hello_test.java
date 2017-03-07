d35ed80e474a966f3f6556a4dde7086275b954ff
lkm <rksym343@naver.com>
2017-03-07 10:32:43 +0900

    로컬에서 추가

--- src/hello_study/hello_test.java
+++ src/hello_study/hello_test.java
@@ -5,6 +5,7 @@ public class hello_test {
 	public static void main(String[] args) {
 		System.out.println("Hello Test");
 		System.out.println("Hello Two Test");
+		System.out.println("로컬에서 추가");
 
 	}
 
